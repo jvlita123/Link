5f1e394e65f610031a02e76a5f7847c44b682df6
Szymon N <sniescior160@gmail.com>
2022-12-08 10:52:33 +0100

    Improved Layout structure

---
+++ Api/wwwroot/js/components/Footer.jsx
@@ -0,0 +1,9 @@
+class Footer extends React.Component {
+    render() {
+        return (
+            <div class="container">
+                &copy; 2022 - Api - <a asp-area="" asp-controller="Home" asp-action="Privacy">Privacy</a>
+            </div>
+        )
+    }
+}
