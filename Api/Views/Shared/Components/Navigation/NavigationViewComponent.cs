7fe916f6c4af64e9940c329f903d58caf2a5b228
Eryk Hordejuk <erykh0@op.pl>
2023-01-26 01:04:58 +0100

    zmiany navbar i naprawienie rejestracji

---
+++ Api/Views/Shared/Components/Navigation/NavigationViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Data.Dto_s.User;
+using Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Service.Services;
+
+namespace Api.Models
+{
+    public class NavigationViewComponent : ViewComponent
+    {
+        private UserService _userService;
+        private AccountService _accountService;
+
+        public NavigationViewComponent(UserService userService, AccountService accountService)
+        {
+            _userService = userService;
+            _accountService = accountService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            string userEmail = HttpContext.User.Identity.Name;
+            NavUserDto? user = null;
+            if (!string.IsNullOrEmpty(userEmail))
+            {
+                Account account = _accountService.GetByEmail(userEmail);
+                user = _userService.GetNavUserDto(account.Id);
+            }
+
+            return View(user);
+        }
+    }
+}
